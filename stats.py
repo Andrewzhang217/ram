eb18b4ec269e90fcebdd0790f2a93913d984f049
Andrewzhang217 <zhangguangzesheng@gmail.com>
2022-08-19 14:20:34 +0800

    Bucket minimizer positions

--- stats.py
+++ stats.py
@@ -1,14 +1,21 @@
-import pandas as pd
+import csv
 import glob
 from scipy.stats import chisquare
-
-#open the csv in bin
 
 path = r'build/bin'
 all_files = glob.glob(path + "/*.csv")
 
-list = []
+buckets = 10
+freq = [None] * buckets
+file_counter = 0
+
 for filename in all_files:
-    df = pd.read_csv(filename)
-    list.append(df)
-print(len(list))
+    print(file_counter)
+    file_counter += 1
+    reader = csv.reader(filename)
+    data = list(reader)
+    seq_len = data[0]
+    for pos in range(1, len(data)):
+        freq[pos * 10 / seq_len] += 1
+
+print(freq)
